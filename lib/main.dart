d6acbb845867fd7044b216f201864debe54920a0
izm51 <tatsuhiko.imaizumi051@gmail.com>
2023-12-04 04:20:19 +0900

    api レスポンスを表示できるように

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,86 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
+import 'package:http/http.dart' as http;
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  late StreamSubscription _intentData;
-  String? data;
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shared Data Example',
+      home: SharedDataScreen(),
+    );
+  }
+}
+
+class SharedDataScreen extends StatefulWidget {
+  @override
+  _SharedDataScreenState createState() => _SharedDataScreenState();
+}
+
+class _SharedDataScreenState extends State<SharedDataScreen> {
+  String _sharedText = '';
+  Future<String>? _responseFuture;
 
   @override
   void initState() {
     super.initState();
 
-    _intentData = ReceiveSharingIntent.getTextStream().listen((String value) {
+    // リアルタイムで共有されたテキストを取得
+    ReceiveSharingIntent.getTextStream().listen((String value) {
       setState(() {
-        data = value;
+        _sharedText = value;
+        _responseFuture = sendTextToAPI(value);
       });
+    }, onError: (err) {
+      print("エラー: $err");
     });
 
+    // 初期共有テキストを取得
     ReceiveSharingIntent.getInitialText().then((String? value) {
-      setState(() {
-        data = value;
-      });
+      if (value != null) {
+        setState(() {
+          _sharedText = value;
+          _responseFuture = sendTextToAPI(value);
+        });
+      }
     });
   }
 
-  @override
-  void dispose() {
-    _intentData.cancel();
-    super.dispose();
+  Future<String> sendTextToAPI(String text) async {
+    // ここでAPIリクエストを行います。適切なURLとパラメーターを指定してください。
+    var response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
+      // body: {'text': text},
+    );
+    return response.body; // APIからのレスポンスを返します
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shared Data'),
+      ),
+      body: Center(
+        child: FutureBuilder<String>(
+          future: _responseFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text("エラー: ${snapshot.error}");
+            } else {
+              return snapshot.hasData
+                  ? Text("APIレスポンス: ${snapshot.data}")
+                  : Text("共有されたテキスト: $_sharedText");
+            }
+          },
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: Text("Sample Title"),
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Shared text:',
-                  ),
-                  Text(
-                    data ?? "",
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
-            )));
+      ),
+    );
   }
 }
