4a763879d04c0b4739c4fc0d82272a68e0a134fc
izm51 <tatsuhiko.imaizumi051@gmail.com>
2023-12-06 22:46:31 +0900

    gpt api からレスポンスを受け取れるように

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
 }
 
@@ -51,12 +55,40 @@ class _SharedDataScreenState extends State<SharedDataScreen> {
   }
 
   Future<String> sendTextToAPI(String text) async {
-    // ここでAPIリクエストを行います。適切なURLとパラメーターを指定してください。
-    var response = await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
-      // body: {'text': text},
+    String openApiKey =
+        dotenv.get('OPENAI_API_KEY'); // FIXME: もっと頻度の低いとこで宣言できるかも
+
+    var response = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: {
+        'Authorization': 'Bearer $openApiKey',
+        'Content-Type': 'application/json'
+        // 'OpenAI-Organization': 'org-aG64VeI3xGVgfoFNU61a18op'
+      },
+      body: jsonEncode({
+        "model": "gpt-3.5-turbo-1106",
+        "response_format": {"type": "json_object"},
+        "messages": [
+          {
+            "role": "system",
+            "content":
+                "ユーザーから与えられた文章から、日程情報を抽出してください。\n\n出力は次のようなJSON形式としてください。\n\n{\n  title: string, // 予定のタイトル\n  start_date: YYYYMMDD, // 開始日\n  start_time: hhmmss, // 開始時刻\n  end_date: YYYYMMDD, // 終了日\n  end_time: hhmmss, // 終了時刻\n  details: string, // 文章の要約。URLは省略しない。改行コードは\"%0A\"を用いる。\n  location: string // 開催場所\n}\n\n注意:\n* 出力にはJSON形式以外の内容を含まないでください。\n* 主催者の名称が分かればtitleに含めてください\n* location は詳しい名称を含めてください"
+          },
+          {"role": "user", "content": text},
+        ],
+        "temperature": 1,
+        "max_tokens": 512,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+      }),
     );
-    return response.body; // APIからのレスポンスを返します
+
+    var jsonResponse = jsonDecode(utf8.decode(response.body.codeUnits))
+        as Map<String, dynamic>;
+
+    return (jsonResponse['choices'] as List).first['message']['content']
+        as String;
   }
 
   @override
