ca84f667c5eed3040352b38e2ed84687b9005e36
izm51 <tatsuhiko.imaizumi051@gmail.com>
2024-01-05 13:00:55 +0900

    fix path (add .env file)

--- functions/functions/.eslintrc.js
+++ functions/functions/.eslintrc.js
@@ -14,10 +14,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: [
-      "functions/functions/tsconfig.json",
-      "functions/functions/tsconfig.dev.json",
-    ],
+    project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
   },
   ignorePatterns: [
