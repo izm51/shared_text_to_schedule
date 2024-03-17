f9f569e7b09fb1cf5f4a766e9e239226dcd92a44
izm51 <tatsuhiko.imaizumi051@gmail.com>
2024-03-17 17:11:52 +0900

    add test

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
+    "jest.config.js",
   ],
   plugins: [
     "@typescript-eslint",
@@ -28,5 +29,6 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "require-jsdoc": 0,
   },
 };
